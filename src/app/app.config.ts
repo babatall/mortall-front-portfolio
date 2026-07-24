c8e065b91118b7e2c804c9921defc0884dba4f0f
mor tall <tallpape32@gmail.com>
2026-07-24 00:46:11 +0000

    Revert "ajout d'un loader global"
    This reverts commit 0837e12755a42d3004dff174029057f32d72c999.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,12 @@ import {provideRouter, withInMemoryScrolling} from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
-import {loadingInterceptor} from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideHttpClient(withInterceptors([loadingInterceptor,authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(
       routes,
       withInMemoryScrolling({
